0a1c8c4628c75f43d72761e1516e67c10f82913d
Rogerio <rogerio.rs@gmail.com>
2016-05-25 08:03:14 -0300

    script para start

--- start.sh
+++ start.sh
@@ -2,4 +2,4 @@
 
 shome=`dirname $0`
 cd "$shome/target"
-java -jar ./safh-0.0.1-SNAPSHOT.war --spring.profiles.active=prod,no-liquibase &
+java -jar ./safh-0.0.1-SNAPSHOT.war --spring.profiles.active=prod &
