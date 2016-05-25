b702af888b0d841e44d494a6808788990ce07a85
Rogerio <rogerio.rs@gmail.com>
2016-05-25 00:53:11 -0300

    ajuste para deploy

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 git pull
+mvn -Pprod
 mvn -Pprod package
--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 shome=`dirname $0`
-java -jar "$shome/target/safh-0.0.1-SNAPSHOT.war --spring.profiles.active=prod,no-liquibase" &
+cd "$shome/target"
+java -jar ./safh-0.0.1-SNAPSHOT.war --spring.profiles.active=prod,no-liquibase &
