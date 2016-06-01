1c5fdeb3e03ad9e61e80485fb77ee1a97d49a310
rogerio.ribeiro <rogerio.ribeiro@m4u.com.br>
2016-06-01 16:32:31 -0300

    Ajustes para deploy e configuração mysql

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-git pull
+mvn clean install
 mvn package -Pprod -DskipTests
+scp -i /home/rogerio/ssh/keyRoger.pem ./target/safh-0.0.1-SNAPSHOT.war ubuntu@52.67.61.182:/home/ubuntu
--- start.sh
+++ start.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-
-shome=`dirname $0`
-cd "$shome/target"
+ssh -i /home/rogerio/ssh/keyRoger.pem ubuntu@52.67.61.182
 java -jar ./safh-0.0.1-SNAPSHOT.war --spring.profiles.active=prod &
